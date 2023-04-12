5bdd2300962ab0038af88066973c708d88bb4f31
MihalevD <mihalev.denislav@gmail.com>
2023-04-12 17:38:15 +0300

    fix ad

--- src/components/Ad.tsx
+++ src/components/Ad.tsx
@@ -1,5 +1,7 @@
 import BasicBox from './basic/BasicBox';
 import styled from '@emotion/styled';
+import {ImageContainer} from './basic/ImageContainer';
+import house from '../img/house.png';
 
 const Wrapper = styled(BasicBox)`
   position: absolute;
@@ -11,6 +13,27 @@ const Wrapper = styled(BasicBox)`
   clip-path: polygon(0 0, 0 100%, 100% 100%, 100% 25%, 89% 0);
   top: -120%;
 `;
+
+const Text = styled.span`
+  text-align: left;
+  font-size: 30px;
+  line-height: 20px;
+  letter-spacing: 0px;
+  color: #cdd5b1;
+  opacity: 1;
+`;
 export const Ad = () => {
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <BasicBox top='20px' style={{marginLeft: '100px'}}>
+        <ImageContainer imageSrc={house} />
+        <BasicBox top='60px'>
+          <Text>
+            С <span style={{fontWeight: 'bold'}}>United Buildings</span> винаги
+            е лесно
+          </Text>
+        </BasicBox>
+      </BasicBox>
+    </Wrapper>
+  );
 };
