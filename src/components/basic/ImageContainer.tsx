369e9c0d34ac07b16206f2cf8c96c97c30f6b2d1
Denislav Mihalev <91677718+MihalevD@users.noreply.github.com>
2023-04-26 22:19:01 +0300

    #2

--- src/components/basic/ImageContainer.tsx
+++ src/components/basic/ImageContainer.tsx
@@ -1,32 +1,42 @@
-import styled from '@emotion/styled';
-import React from 'react';
-import Colors from '../../tokens/Colors';
-import BasicBox from './BasicBox';
-import {TextContainer} from './TextContainer';
+import styled from "@emotion/styled";
+import React from "react";
+import Colors from "../../tokens/Colors";
+import BasicBox from "./BasicBox";
+import { TextContainer } from "./TextContainer";
 
 const tagTextStyles = {
-  color: '#C9C7C7',
-  fontSize: '30px',
-  lineHeight: '20px',
-  fontWeight: '700',
-  alignText: 'left' as const,
+  color: "#C9C7C7",
+  fontSize: "30px",
+  lineHeight: "20px",
+  fontWeight: "700",
+  alignText: "left" as const,
 };
 
 const boxStyle = {
-  position: 'absolute' as const,
-  bottom: '15px',
-  left: '26px',
+  position: "absolute" as const,
+  bottom: "15px",
+  left: "26px",
 };
 
 export const ImageContainer: React.FC<{
-  imageStyles?: {[name: string]: string};
+  imageStyles?: { [name: string]: string };
   imageSrc?: string;
   imgText?: string;
   fullWidth?: boolean;
   fullHeight?: boolean;
   tag?: string;
-  style?: {[name: string]: string};
-}> = ({imageSrc, imageStyles, fullWidth, fullHeight, imgText, tag, style}) => {
+  style?: { [name: string]: string };
+  children?: React.ReactNode;
+}> = ({
+  imageSrc,
+  imageStyles,
+  fullWidth,
+  fullHeight,
+  imgText,
+  tag,
+  style,
+  children,
+}) => {
   return (
     <Wrapper fullWidth={fullWidth} fullHeight={fullHeight}>
       {!!imageSrc && (
@@ -37,13 +47,15 @@ export const ImageContainer: React.FC<{
         />
       )}
 
+      {!!children && children}
+
       {imgText && (
         <TextContainer
           text={imgText}
           customStyles={{
-            position: 'absolute',
-            bottom: '10px',
-            left: '10px',
+            position: "absolute",
+            bottom: "10px",
+            left: "10px",
           }}
         />
       )}
@@ -60,15 +72,16 @@ const Wrapper = styled(BasicBox)`
 `;
 
 export const ImageComponent: React.FC<{
-  imageStyles?: {[name: string]: string};
+  imageStyles?: { [name: string]: string };
   imageSrc: string;
-  outerStyle?: {[name: string]: string};
-}> = ({imageSrc, imageStyles, outerStyle}) => {
+  outerStyle?: { [name: string]: string };
+}> = ({ imageSrc, imageStyles, outerStyle }) => {
   return (
     <img
       src={imageSrc}
-      alt={'static img'}
+      alt={"static img"}
       {...imageStyles}
-      style={outerStyle}></img>
+      style={outerStyle}
+    ></img>
   );
 };
--- src/components/pages/PropertyPage.tsx
+++ src/components/pages/PropertyPage.tsx
@@ -1,12 +1,14 @@
-import {useParams} from 'react-router-dom';
-import BasicBox from '../basic/BasicBox';
-import {useState} from 'react';
-import {items} from '../../helper/constants';
-import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
-import {TextContainer} from '../basic/TextContainer';
-import styled from '@emotion/styled';
-import {ImageContainer} from '../basic/ImageContainer';
-import bkg from '../../img/apart2.jpg';
+import { useParams } from "react-router-dom";
+import BasicBox from "../basic/BasicBox";
+import { useState } from "react";
+import { items } from "../../helper/constants";
+import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
+import { TextContainer } from "../basic/TextContainer";
+import styled from "@emotion/styled";
+import { ImageContainer } from "../basic/ImageContainer";
+import bkg from "../../img/apart2.jpg";
+import apart from "../../img/apart.jpg";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 
 const CustomText = styled(TextContainer)`
   text-align: left;
@@ -14,33 +16,167 @@ const CustomText = styled(TextContainer)`
   letter-spacing: 0px;
   color: #000000;
 `;
+
+const SmallImg = styled(BasicBox)`
+  width: 251px;
+  height: 143px;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  border: 1px solid #707070;
+  border-radius: 20px;
+  position: absolute;
+  right: 5%;
+  bottom: 5%;
+  cursor: pointer;
+  :before {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 101;
+    border-radius: 20px;
+    transition: background-color 0.5s;
+  }
+  :hover:before {
+    background-color: rgba(0, 0, 0, 0.7);
+  }
+`;
+
+const Custom = styled(BasicBox)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 102;
+  width: 80%;
+`;
+
+const InsideButtonText = styled(TextContainer)`
+  text-align: center;
+  font: normal normal 600 20px/27px Segoe UI;
+  letter-spacing: 0px;
+  color: #3f4554;
+  opacity: 1;
+  color: #ffffff;
+`;
+const textStylesHeading = {
+  font: "normal normal bold 30px/40px Segoe UI",
+  letterSpacing: "0px",
+  color: "#3F4554",
+  opacity: "1",
+};
+
+const textStylesHeading2 = {
+  textАlign: "left" as const,
+  font: "normal normal bold 20px/12px Segoe UI",
+  letterSpacing: "0px",
+  color: "#3F4554",
+};
+
+const textStylesSubHeading = {
+  textAlign: "center" as const,
+  font: "normal normal 600 24px/32px Segoe UI",
+  letterSpacing: "0px",
+  color: "#3F4554",
+};
+
+const AdWrapper = styled(BasicBox)`
+  box-sizing: border-box;
+  width: 30%;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  box-shadow: 0px 3px 6px #00000029;
+  border-radius: 30px;
+  opacity: 1;
+`;
 export const PropertyPage = () => {
-  const {id} = useParams();
-  const [data, setData] = useState(items.find(elem => elem.id === Number(id)));
+  const { id } = useParams();
+  const [data, setData] = useState(
+    items.find((elem) => elem.id === Number(id))
+  );
 
   const goBack = () => {
     window.history.back();
   };
   return (
     <BasicBox
-      left='100px'
-      right='100px'
+      left="100px"
+      right="100px"
       fullWidth
-      style={{boxSizing: 'border-box'}}
-      direction='column'
-      bottom='500px'>
-      <BasicBox onClick={goBack} align='center'>
-        <KeyboardBackspaceIcon /> <CustomText text={'Назад'} />
+      style={{ boxSizing: "border-box" }}
+      direction="column"
+      bottom="500px"
+    >
+      <BasicBox
+        onClick={goBack}
+        align="center"
+        top="15px"
+        bottom="15px"
+        style={{ cursor: "pointer" }}
+      >
+        <KeyboardBackspaceIcon /> <CustomText text={"Назад"} />
       </BasicBox>
       <BasicBox fullWidth>
         <ImageContainer
           imageSrc={bkg}
+          fullWidth
           style={{
-            width: '100%',
-            borderRadius: '30px',
-            clipPath: 'polygon(0 0, 0 100%, 100% 100%, 100% 25%, 89% 0)',
+            width: "100%",
+            borderRadius: "30px",
+            maxHeight: "550px",
+            clipPath: "polygon(0 0, 0 100%, 100% 100%, 100% 25%, 89% 0)",
           }}
+          imageStyles={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+          }}
+          children={
+            <SmallImg>
+              <ImageContainer
+                imageSrc={apart}
+                fullWidth
+                fullHeight
+                style={{ borderRadius: "20px" }}
+                imageStyles={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+              ></ImageContainer>
+              <Custom>
+                <InsideButtonText text={"Разгледай всички 5 снимки"} />
+              </Custom>
+            </SmallImg>
+          }
         />
+      </BasicBox>
+      <BasicBox top="33px" fullWidth justify="space-between">
+        <BasicBox direction="column">
+          <TextContainer
+            text={data ? data.title : "Чудесен едностаен апартамент"}
+            textStyles={textStylesHeading}
+          />
+          <BasicBox top="16px">
+            <LocationOnIcon width="36px" />
+            <TextContainer
+              customStyles={{ marginLeft: "10px" }}
+              text={data ? data.address : "No address"}
+              textStyles={textStylesSubHeading}
+            />
+          </BasicBox>
+        </BasicBox>
+        <AdWrapper direction="column" spacing="24px" fullPadding>
+          <BasicBox bottom="21px">
+            <TextContainer
+              text={"Кратко описание"}
+              textStyles={textStylesHeading2}
+            />
+          </BasicBox>
+          <BasicBox bottom="21px"></BasicBox>
+        </AdWrapper>
       </BasicBox>
     </BasicBox>
   );
