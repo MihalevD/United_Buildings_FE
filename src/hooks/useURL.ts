e0e935f9d655fd6a5f77e11e30a6cf826ae4f486
Denislav Mihalev <91677718+MihalevD@users.noreply.github.com>
2023-09-06 18:29:46 +0300

    why

--- src/hooks/useURL.ts
+++ src/hooks/useURL.ts
@@ -1,4 +1,5 @@
 export default function useUrl() {
+    console.log(import.meta.env.VITE_NODE_ENV)
     const url_name = import.meta.env.VITE_NODE_ENV === 'production' ? 'https://united-buildings-be.online' : 'http://localhost:3000';
     console.log(url_name)
     return url_name;
