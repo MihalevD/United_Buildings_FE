d5237d16570b36dc14bcd464e83d282bc22d9e77
Denislav Mihalev <91677718+MihalevD@users.noreply.github.com>
2023-09-06 20:44:46 +0300

    fx

--- src/hooks/useURL.ts
+++ src/hooks/useURL.ts
@@ -1,6 +1,6 @@
 export default function useUrl() {
     console.log(import.meta.env.VITE_NODE_ENV)
-    const url_name = import.meta.env.VITE_NODE_ENV === 'production' ? 'http://united-buildings-be.online' : 'http://localhost:3000';
+    const url_name = import.meta.env.VITE_NODE_ENV === 'production' ? 'https://united-buildings-be.online' : 'http://localhost:3000';
     console.log(url_name)
     return url_name;
 }
