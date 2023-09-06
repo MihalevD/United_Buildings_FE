06489e0e3223060c2a253410c5c6da3df3d193f9
Denislav Mihalev <91677718+MihalevD@users.noreply.github.com>
2023-09-06 18:12:25 +0300

    test 2

--- src/hooks/useURL.ts
+++ src/hooks/useURL.ts
@@ -1,4 +1,5 @@
 export default function useUrl() {
+    console.log(process.env.Node_ENV)
     const url_name = process.env.Node_ENV === 'production' ? 'https://united-buildings-be.online' : 'http://localhost:3000';
     console.log(url_name)
     return url_name;
